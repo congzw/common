dbeb7aca84d6a4a11b171aaae4e907f6d41a4c85
congzw <2981810300@qq.com>
2020-07-24 14:39:55 +0800

    add log helper

---
+++ src/Common.Logs/Logs/LogHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Common.Logs
+{
+    public interface ILogHelper
+    {
+        void Log(string message, int level);
+    }
+
+    public class LogHelper : ILogHelper
+    {
+        public void Log(string message, int level = 1)
+        {
+            var logMessage = string.Format("{0} [{1}]{2} {3}", "LogCenter.Common.LogHelper", "Default", level.ToString(), message);
+            Trace.WriteLine(logMessage);
+        }
+
+        #region for extensions and simple use
+
+        private static readonly Lazy<LogHelper> _lazy = new Lazy<LogHelper>(() => new LogHelper());
+        public static ILogHelper Instance => ServiceLocator.Current.GetService<ILogHelper>() ?? _lazy.Value;
+        public static void Debug(string message)
+        {
+            Instance.Debug(message);
+        }
+
+        #endregion
+    }
+}
---
+++ src/Common.Logs/Logs/LogHelperExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Common.Logs
+{
+    public static class LogHelperExtensions
+    {
+        public static void Trace(this ILogHelper logHelper, string message)
+        {
+            logHelper.Log(message, 0);
+        }
+        public static void Debug(this ILogHelper logHelper, string message)
+        {
+            logHelper.Log(message, 1);
+        }
+        public static void Info(this ILogHelper logHelper, string message)
+        {
+            logHelper.Log(message, 2);
+        }
+        public static void Warn(this ILogHelper logHelper, string message)
+        {
+            logHelper.Log(message, 3);
+        }
+        public static void Error(this ILogHelper logHelper, Exception ex, string message)
+        {
+            var logMessage = string.Format("{0}=>{1}", message, ex?.StackTrace);
+            logHelper.Log(logMessage, 4);
+        }
+        public static void Fatal(this ILogHelper logHelper, Exception ex, string message)
+        {
+            var logMessage = string.Format("{0}=>{1}", message, ex?.StackTrace);
+            logHelper.Log(logMessage, 5);
+        }
+    }
+}
